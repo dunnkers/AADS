34bfbeb5077c7290c96317c7a061ad7177ec25f7
dunnkers <jeroenoverschie@gmail.com>
2018-03-20 12:06:46 +0100

    A working (not efficient) queue implementation 😇 Runtime error on case 3 though. Shouldn't use a queue probably.

--- COLOR/color.py
+++ COLOR/color.py
@@ -7,6 +7,7 @@ import os
 import sys
 import math
 from datetime import datetime
+from queue import Queue
 startTime = datetime.now()
 
 # LOGGING
@@ -20,24 +21,53 @@ if "TEST" in os.environ:
     sys.stdin = open('./COLOR/4.in')
 
 def findColors(graph):
-    colors = {}
-    color_1 = 0
-    color_2 = 0
+    red_colored = 0
+
+    while len(graph) > 0:
+        # since we only have 2 colors, we only have to invert. we choose True and
+        # False since they're easy to invert.
+        colors = {
+            True: 0,  # a color, e.g. red
+            False: 0  # a color, e.g. blue
+        }
+
+        queue = Queue() # might've been a priority queue based on valency --
+        queue.put((True, next(iter(graph.keys())))) # arbitrary item
+        while not queue.empty(): # do a coloring
+            # dequeue
+            item = queue.get()
+            color = item[0]
+            node = item[1]
+
+            # coloring
+            colors[color] += 1
+            neighbors = graph.pop(node)
+            for neighbor in neighbors:
+                if neighbor in graph:
+                    queue.put((not color, neighbor))
+        red_colored += max(colors.values())  # possibly invert drawing
     
-    def check_neighbors(clr, vertices):
-        return all(clr != colors.get(n) for n in vertices)
+    return red_colored
+    # # first node
+    # # colors[node] = 0
+    
+    # color_1 = 0
+    # color_2 = 0
+    
+    # def check_neighbors(clr, vertices):
+    #     return all(clr != colors.get(n) for n in vertices)
 
-    for node, adj_vertices in graph.items():
-        if check_neighbors(0, adj_vertices): # all neighbors are not color 0
-            colors[node] = 0
-            color_1 += 1
-        elif check_neighbors(1, adj_vertices):  # all neighbors are not color 0
-            colors[node] = 1
-            color_2 += 1
-        else:
-            return None # cannot find any color for this vertex
+    # for node, adj_vertices in graph.items():
+    #     if check_neighbors(0, adj_vertices): # all neighbors are not color 0
+    #         colors[node] = 0
+    #         color_1 += 1
+    #     elif check_neighbors(1, adj_vertices):  # all neighbors are not color 0
+    #         colors[node] = 1
+    #         color_2 += 1
+    #     else:
+    #         return None # cannot find any color for this vertex
 
-    return max(color_1, color_2)
+    # return max(color_1, color_2)
 
 t = int(input())
 for _ in range(t):
@@ -53,6 +83,7 @@ for _ in range(t):
         graph[a].append(b)
         graph[b].append(a)
     lonely_vertices = m - len(graph)
+    
     colors = findColors(graph)
     if colors:
         print(colors + lonely_vertices)
