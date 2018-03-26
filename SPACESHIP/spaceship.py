1e272599659cf3915c63ae67ee9d5efd66ed90d1
dunnkers <jeroenoverschie@gmail.com>
2018-03-26 04:18:53 +0200

    Refactoring SPACESHIP (1)

--- SPACESHIP/spaceship.py
+++ SPACESHIP/spaceship.py
@@ -14,7 +14,6 @@ def printdebug(*s):
     if "TEST" in os.environ:
         print(*s)
 
-
 # REDIRECT STDIN
 if "TEST" in os.environ:
     old_stdin = sys.stdin
@@ -30,91 +29,76 @@ for _ in range(n - 1):  # scan weighted edges
     tree[a][b] = bool(c)
     tree[b][a] = bool(c)
 
-def visit(tree, node, visited=set()):
-    visited.add(node)
-    for neighbor, arg in tree[node].items():
-        if neighbor in visited:
-            continue
-        yield neighbor, arg
-
 class Spaceship(object):
-    def __init__(self, spaceship):
+    def __init__(self, spaceship, k):
         self.spaceship = spaceship
+        self.k = k
         self.pressure = 1
         self.gates_closed = 0
 
-    def yield_edges(self, q):
+    def yield_edges(self, q, visited=set()):
         while q:
             v = q.pop()
-            for w, closable in visit(self.spaceship, v):
-                yield v, w, closable
+            visited.add(v)
+            for w, closable in self.spaceship[v].items():
+                if w in visited:
+                    continue
+                yield w, closable
 
-    def check(self, k):
-        return self.pressure + 1 > k
+    def check(self, potential):
+        return self.pressure + potential > self.k
 
-    def travel(self, k):
+    def travel(self):
         gates = deque()
         q = deque([ 1 ])
 
-        for _, w, closable in self.yield_edges(q):
+        # EXPLORE ALL NODES BEFORE GATES
+        for w, closable in self.yield_edges(q):
             if closable:
                 gates.append(w)
-            elif self.pressure + 1 > k: # would explode
+            elif self.check(1):  # would explode
                 printdebug('exploding at', w)
                 return -1
             else:
                 q.append(w)
                 printdebug(w, closable)
                 self.pressure += 1
-        printdebug('→ initial pressure',self.pressure)
+        printdebug('→ initial pressure', self.pressure)
 
+        # EXPLORE WHICH GATES NEED TO BE CLOSED
         full_potential = 0
         gateque = []
         while gates:
             gate = gates.pop()
             printdebug('GATE', gate)
             potential = 1
-            if self.pressure + potential > k:
-                printdebug('closing gate', gate, 'before inspection')
-                self.gates_closed += 1
-                continue
             gatequeue = deque([ gate ])
-            for _, w, closable in self.yield_edges(gatequeue):
+            for w, closable in self.yield_edges(gatequeue):
                 potential += 1
                 printdebug('🔶', w)
-                if self.pressure + potential > k:
+                if self.check(potential):
                     printdebug('closing gate', gate, 'on inspection at', w)
                     self.gates_closed += 1
                     break
                 gatequeue.append(w)
             else: # if break was not triggered in `for`
                 printdebug('gate', gate, 'has', potential, 'potential pressure')
-                heappush(gateque, (-potential, gate))
+                heappush(gateque, (-potential, gate)) # *-1 for max-heap
                 full_potential += potential
-        # maxpress = self.pressure + full_potential
-        # while maxpress > k:
-        #     if not gateque:
-        #         printdebug('this should not happen...')
-        #         return -1
-        #     potential, gate = heappop(gateque)
-        #     potential *= -1
-        #     maxpress -= potential
-        #     self.gates_closed += 1
+
+        # EXPLORE HOW MANY GATES WE MINIMALLY HAVE TO CLOSE
         while gateque:
             potential, gate = heappop(gateque)
-            potential *= -1
-            if self.pressure + full_potential > k:
+            potential *= -1 # revert back to positive no
+            if self.check(full_potential):
                 self.gates_closed += 1
                 full_potential -= potential
                 printdebug('closing gate', gate)
             else:  # leaving gate open
                 printdebug('leaving open', gate)
-                # self.pressure += potential
-            printdebug('→ pressure', self.pressure)
 
         return self.gates_closed
 
-# block 1 is depressurised first
-spaceship = Spaceship(tree)
-gates = spaceship.travel(k)
+spaceship = Spaceship(tree, k)
+gates = spaceship.travel()
 print(gates)
