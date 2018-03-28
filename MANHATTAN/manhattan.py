cbb3980d261e555b05fac25661c6a780a34f1351
dunnkers <jeroenoverschie@gmail.com>
2018-03-28 16:04:35 +0200

    Start building spots ✌🏻

--- MANHATTAN/manhattan.py
+++ MANHATTAN/manhattan.py
@@ -21,8 +21,8 @@ if "TEST" in os.environ:
 # SCAN INPUT
 [n, m] = [int(x) for x in input().split()]  # rows, columns
 matrix = []
-# graph = {}          # for finding buildings efficiently
-stones = set()      # stones, e.g. building pieces
+stones = set()          # stones, e.g. building pieces
+spots = set()           # empty spots e.g. potential ice-cream places
 for i in range(n):
     row = [int(x) for x in input().split()]
     matrix.append(row)
@@ -30,6 +30,8 @@ for i in range(n):
     for j, cell in enumerate(row):  # store stones, e.g. building pieces
         if cell == 1:               # its a stone
             stones.add((i, j))
+        else:                       # its a spot
+            spots.add((i, j))
 
 def peek(i, j):
     return None if i < 0 or i > n - 1 or j < 0 or j > m - 1 else (i, j)
@@ -50,6 +52,7 @@ def construct(root): # recursively construct a building from 1 stone
     building = set([root])
     q = deque([root])
     while q:
+        # (!) might be more efficient to just do a for loop here ...
         neighs = filter(lambda neigh: neigh not in building and isStone(neigh), 
             neighbors(q.pop()))
         building = building.union(set(list(neighs)))
@@ -66,4 +69,8 @@ while stones:
     stones -= building # remove this building from building stones
     buildings.append(building)
 
+while spots:
+    spot = spots.pop()
+    printdebug('SPOT', spot)
+    break
 print(-1)
